3b85545fc71be2a1973d95b30451d815fac3f6d7
Brian <okellobrian18@gmail.com>
2025-04-12 09:17:26 +0300

    Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <h1>
+        Expense Tracker
+      </h1>
+
+      <p>
+        Start taking control of your finances and Life.
+        Record, Categorize and analyze your spending.
+      </p>
+    </div>
   )
 }
 
